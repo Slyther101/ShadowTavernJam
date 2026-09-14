2fcc2f5c4154c3a67a12b2114def022afe0d11ce
CLAAAY <git@claytaeto.com>
2026-09-13 20:53:19 -0700

    add attack

--- game-jam-2026-wayward-sheep-metroidvania/Scripts/Controler.gd
+++ game-jam-2026-wayward-sheep-metroidvania/Scripts/Controler.gd
@@ -7,24 +7,35 @@ extends CharacterBody2D
 var speed_multiplier = 30.0
 var jump_multiplier = -30.0
 var direction = 0
+var attack_step := 0
+var attack_queued := false
 
 #const SPEED = 300.0
 #const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("Attack"):
+		if attack_step == 0:
+			attack_step = 1
+			hero_animation.play("Attack")
+		elif attack_step == 1:
+			attack_queued = true
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor() and not attack_step:
 		velocity.y = jump_power * jump_multiplier
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if attack_step:
+		velocity.x = 0
+	elif direction:
 		velocity.x = direction * speed * speed_multiplier
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed * speed_multiplier)
@@ -34,6 +45,18 @@ func _physics_process(delta: float) -> void:
 
 
 func _update_animation() -> void:
+	if attack_step:
+		if not hero_animation.is_playing():
+			if attack_queued and attack_step == 1:
+				attack_step = 2
+				attack_queued = false
+				hero_animation.play("Attack 2")
+				return
+			else:
+				attack_step = 0
+		else:
+			return
+
 	var animation := "Jump" if not is_on_floor() else "Run" if direction else "Idle"
 	if hero_animation.animation != animation:
 		hero_animation.play(animation)
