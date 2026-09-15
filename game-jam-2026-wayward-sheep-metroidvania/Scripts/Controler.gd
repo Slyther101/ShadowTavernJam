09539754c7847eb31e2f350da0590e961e39990b
CLAAAY <git@claytaeto.com>
2026-09-15 10:31:08 -0700

    first pass on additional actions

--- game-jam-2026-wayward-sheep-metroidvania/Scripts/Controler.gd
+++ game-jam-2026-wayward-sheep-metroidvania/Scripts/Controler.gd
@@ -4,64 +4,110 @@ extends CharacterBody2D
 @export var jump_power = 10.0
 @onready var hero_animation: AnimatedSprite2D = $HeroAnimation
 
+const DASH_DURATION := 0.15
+const SHADOW_DASH_DURATION := 0.20
+const SLIDE_DURATION := 0.25
+
 var speed_multiplier = 30.0
 var jump_multiplier = -30.0
-var direction = 0
-var attack_step := 0
+var direction := 0.0
+var action_name: StringName
+var action_time := 0.0
+var action_multiplier := 0.0
+var action_direction := 1.0
 var attack_queued := false
-
-#const SPEED = 300.0
-#const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("Attack"):
-		if attack_step == 0:
-			attack_step = 1
-			hero_animation.play("Attack")
-		elif attack_step == 1:
-			attack_queued = true
-
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor() and not attack_step:
+	direction = Input.get_axis("Move Left", "Move Right")
+	if direction:
+		hero_animation.flip_h = direction < 0
+
+	if action_name == &"Attack" and Input.is_action_just_pressed("Attack"):
+		attack_queued = true
+
+	if action_name:
+		_update_action(delta)
+	else:
+		_try_start_action()
+		if action_name:
+			_update_action(delta)
+		else:
+			_update_movement()
+
+	move_and_slide()
+	if not action_name:
+		_update_animation()
+
+
+func _try_start_action() -> void:
+	if Input.is_action_just_pressed("Test Die"):
+		_start_action(&"Die")
+	elif Input.is_action_just_pressed("Test Damage"):
+		_start_action(&"Damage")
+	elif Input.is_action_just_pressed("Test Push"):
+		_start_action(&"Push")
+	elif Input.is_action_just_pressed("Summon Shadow"):
+		_start_action(&"SummonShadow")
+	elif Input.is_action_just_pressed("Shadow Strike"):
+		_start_action(&"ShadowStrike")
+	elif Input.is_action_just_pressed("Attack"):
+		_start_action(&"Attack" if is_on_floor() else &"AirStrike")
+	elif Input.is_action_just_pressed("Dash") and is_on_floor():
+		_start_action(&"Dash", DASH_DURATION, 2.0)
+	elif Input.is_action_just_pressed("Shadow Dash") and is_on_floor():
+		_start_action(&"ShadowDash", SHADOW_DASH_DURATION, 3.0)
+	elif Input.is_action_just_pressed("Slide") and is_on_floor():
+		_start_action(&"Slide", SLIDE_DURATION, 1.0)
+	elif Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = jump_power * jump_multiplier
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	direction = Input.get_axis("ui_left", "ui_right")
-	if attack_step:
-		velocity.x = 0
-	elif direction:
+
+func _start_action(animation: StringName, duration := 0.0, multiplier := 0.0) -> void:
+	action_name = animation
+	action_time = duration
+	action_multiplier = multiplier
+	action_direction = direction if direction else -1.0 if hero_animation.flip_h else 1.0
+	hero_animation.play(animation)
+
+
+func _update_action(delta: float) -> void:
+	if action_time > 0.0:
+		action_time -= delta
+		velocity.x = action_direction * speed * speed_multiplier * action_multiplier
+		if action_time <= 0.0:
+			action_name = &""
+			velocity.x = 0.0
+			hero_animation.stop()
+		return
+
+	velocity.x = 0.0
+	if hero_animation.is_playing():
+		return
+	if action_name == &"Die":
+		get_tree().reload_current_scene()
+		return
+	if action_name == &"Attack" and attack_queued:
+		attack_queued = false
+		_start_action(&"Attack 2")
+		return
+	action_name = &""
+
+
+func _update_movement() -> void:
+	if direction:
 		velocity.x = direction * speed * speed_multiplier
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed * speed_multiplier)
 
-	move_and_slide()
-	_update_animation()
-
 
 func _update_animation() -> void:
-	if attack_step:
-		if not hero_animation.is_playing():
-			if attack_queued and attack_step == 1:
-				attack_step = 2
-				attack_queued = false
-				hero_animation.play("Attack 2")
-				return
-			else:
-				attack_step = 0
-		else:
-			return
-
 	var animation := "Jump" if not is_on_floor() else "Run" if direction else "Idle"
 	if hero_animation.animation != animation:
 		hero_animation.play(animation)
-	if direction:
-		hero_animation.flip_h = direction < 0
 
 
 		
